75d9ee7e47eab2dcea4023da938357997fc5b411
Vidyesh Churi <vidyesh95@gmail.com>
2024-10-28 22:09:05 +0530

    applied font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,27 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
+import type {Metadata} from "next";
+import {Inter} from "next/font/google";
+import {Roboto_Serif} from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({subsets: ["latin"], variable: "--font-inter"});
+const robotoSerif = Roboto_Serif({subsets: ["latin"], variable: "--font-roboto-serif"});
 
 export const metadata: Metadata = {
-  title: {
-    template: "%s | Chinsong",
-    absolute: "Chinsong",
-  },
-  description: "Free music with lyrics.",
+    title: {
+        template: "%s | Chinsong",
+        absolute: "Chinsong",
+    },
+    description: "Free music with lyrics.",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+export default function RootLayout(
+    {children,}: Readonly<{ children: React.ReactNode; }>
+) {
+    return (
+        <html lang="en">
+        <body className={`${inter.variable}  ${robotoSerif.variable} font-normal antialiased`}>
         {children}
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
